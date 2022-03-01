2e6ffc4dfa5085c0a4fb0cbc48b60f28ca5098d2
Alex Luibeznov <dr3amyh3ad@gmail.com>
2022-03-01 21:30:04 +0300

    create toolbar

---
+++ frontend/src/app/toolbar/toolbar.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+
+import { ToolbarComponent } from './toolbar.component';
+
+@NgModule({
+  declarations: [
+    ToolbarComponent
+  ],
+  exports: [
+    ToolbarComponent
+  ]
+})
+
+export class ToolbarModule { }
