903f56122867df1eb3397cb439c261352e4b3bdf
Alex Luibeznov <dr3amyh3ad@gmail.com>
2022-03-06 20:15:43 +0300

    added date

--- frontend/src/app/toolbar/toolbar.component.ts
+++ frontend/src/app/toolbar/toolbar.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { ToolbarService } from './toolbar.service';
-import { Weather } from './toolbar.service';
+import { ToolbarService, Weather } from './toolbar.service';
 import { delay } from 'rxjs/operators';
+
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
@@ -13,8 +13,6 @@ export class ToolbarComponent{
 
   city: string;
   weather: Weather;
-  // sunriseTime: string;
-  // sunsetTime: string;
   @Output() addWeather: EventEmitter<Weather> = new EventEmitter<Weather>();
 
   constructor(private toolbarService: ToolbarService) {}
@@ -22,14 +20,9 @@ export class ToolbarComponent{
   sendRequest(city: string) {
     this.toolbarService.sendRequest(city)
         .subscribe(response =>{
-          console.log(this.city);
-          console.log(response);
           this.weather = response;
           this.addWeather.emit(this.weather);
-          // let date = new Date(this.weather.sys.sunrise * 1000);
-          // this.sunriseTime = date.toLocaleString('ru-RU', {hour: 'numeric', minute: 'numeric'});
-          // date = new Date(this.weather.sys.sunset * 1000);
-          // this.sunsetTime = date.toLocaleString('ru-RU', {hour: 'numeric', minute: 'numeric'});
+          this.city = '';
     });
   }
 
